af0cf8996638997b9979b96bbe026dac432e8b74
ExpansiveGold <61693862+ExpansiveGold@users.noreply.github.com>
2021-11-09 10:33:35 -0300

    Update bancodedados.js

--- config/bancodedados.js
+++ config/bancodedados.js
@@ -2,7 +2,7 @@
 var mongoose = require('mongoose');
 
 //configurar o bando de dados
-var conexao = async()=>{
+var conexao = ()=>{
     mongoose.connect('mongodb+srv://ExpansiveGold:MongoPass@cluster0.6hqym.mongodb.net/ATweb?retryWrites=true&w=majority')
 };
 
