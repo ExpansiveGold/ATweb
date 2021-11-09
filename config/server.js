a12850c5b4e4426d85100a4014d7e242b649af6f
ExpansiveGold <61693862+ExpansiveGold@users.noreply.github.com>
2021-11-09 11:18:51 -0300

    Add files via upload

--- config/server.js
+++ config/server.js
@@ -17,7 +17,7 @@ app.use(express.static('./web/assets'))
 app.use(express.urlencoded({extended: true}))
 
 //definir a porta do servidor
-var porta = process.env.PORT
+var porta = process.env.PORT || 1313
 
 // configurar as pastas para o carregamento automático
 consign().include('./web/routes').into(app)
